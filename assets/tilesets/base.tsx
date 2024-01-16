2e8e53d9b76522e1c5e6fe4ce8845f9264044eba
Trey Tomes <george.tomes@cityelectricsupply.com>
2024-01-16 11:57:00 -0600

    Load in 2 tile layers.  Animate the deco layer.

---
+++ assets/tilesets/base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="base" tilewidth="16" tileheight="16" tilecount="2091" columns="41">
+ <image source="../images/tile_base.png" width="656" height="816"/>
+</tileset>
